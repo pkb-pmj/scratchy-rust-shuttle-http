b0fb38756c8cb98c1df3baed7eafaf559c106533
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-05-07 18:21:13 +0200

    ✨ create locale embed traits

--- src/locales/mod.rs
+++ src/locales/mod.rs
@@ -1,4 +1,5 @@
 use i18n_codegen::i18n;
+use twilight_util::builder::embed::{EmbedBuilder, EmbedFieldBuilder};
 
 i18n!("src/locales");
 
@@ -19,4 +20,12 @@ impl From<Option<String>> for Locale {
 
 pub trait ToLocaleString {
     fn to_locale_string(&self, locale: Locale) -> String;
+}
+
+pub trait ToLocaleEmbed {
+    fn to_locale_embed(&self, locale: Locale) -> EmbedBuilder;
+}
+
+pub trait ToLocaleEmbedField {
+    fn to_locale_embed_field(&self, locale: Locale) -> EmbedFieldBuilder;
 }
