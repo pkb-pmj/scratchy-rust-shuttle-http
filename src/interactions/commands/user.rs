c221d122cb317820f23032f06b170d06eae158a6
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-05-16 11:14:57 +0200

    🚧 🥅 API error handling in /user

--- src/interactions/commands/user.rs
+++ src/interactions/commands/user.rs
@@ -52,20 +52,88 @@ pub async fn run(
         _ => unreachable!("expected option 'username' to be of type String"),
     };
 
-    let (api_user, db_user) = tokio::join!(
+    let (api, db) = tokio::join!(
         state.client.get::<api::User>(username.to_string()),
         state.client.get::<db::User>(username.to_string()),
     );
 
     let mut embed = EmbedBuilder::new().color(Color::Success.into());
 
-    if let Ok(user) = api_user {
-        embed = user.extend_locale_embed(locale, embed);
-    }
+    // embed = match result {
+    //     (Err(ScratchAPIError::NotFound), Err(ScratchAPIError::NotFound)) => embed
+    //         .title(locale.error_not_found())
+    //         .description(
+    //             locale.error_not_found_user(&format!("https://scratch.mit.edu/user/{username}")),
+    //         )
+    //         .footer(APIStatus::Both),
+    //     (Err(ScratchAPIError::Other(_)), Err(ScratchAPIError::Other(_))) => {
+    //         embed.footer(APIStatus::None)
+    //     }
+    //     (Ok(api), Err(ScratchAPIError::Other(_))) => api
+    //         .extend_locale_embed(locale, embed)
+    //         .footer(APIStatus::OnlyAPI),
+    //     (Err(ScratchAPIError::Other(_)), Ok(db)) => db
+    //         .extend_locale_embed(locale, embed)
+    //         .footer(APIStatus::OnlyDB),
+    //     (api, db) => {
+    //         if let Ok(user) = api {
+    //             embed = user.extend_locale_embed(locale, embed);
+    //         }
+    //         if let Ok(user) = db {
+    //             embed = user.extend_locale_embed(locale, embed);
+    //         }
+    //         embed
+    //     } // (Ok(api), Ok(db)) => db
+    //       //     .extend_locale_embed(locale, api.extend_locale_embed(locale, embed))
+    //       //     .footer(APIStatus::Both),
+    //       // (Ok(api), Err(db)) => api
+    //       //     .extend_locale_embed(locale, embed)
+    //       //     .footer(APIStatus::OnlyAPI),
+    //       // (Err(api), Ok(db)) => db
+    //       //     .extend_locale_embed(locale, embed)
+    //       //     .footer(APIStatus::OnlyDB),
+    //       // (Err(api), Err(db)) => {
+    //       //     embed = embed.color(Color::Error.into());
+    //       //     match (api, db) {
+    //       //         (ScratchAPIError::Other(_), ScratchAPIError::Other(_)) => {
+    //       //             embed.footer(APIStatus::None)
+    //       //         }
+    //       //         _ => embed
+    //       //             .title(locale.error_not_found())
+    //       //             .description(locale.error_not_found_user(&format!(
+    //       //                 "https://scratch.mit.edu/user/{username}"
+    //       //             )))
+    //       //             .footer(APIStatus::Both),
+    //       //     }
+    //       // }
+    // };
+    embed = match api {
+        Ok(user) => {
+            embed = user.extend_locale_embed(locale, embed);
 
-    if let Ok(user) = db_user {
-        embed = user.extend_locale_embed(locale, embed);
-    }
+            match db {
+                Ok(user) => user.extend_locale_embed(locale, embed),
+                Err(_) => embed,
+            }
+        }
+        Err(error) => match error {
+            ScratchAPIError::NotFound => embed
+                .color(Color::Error.into())
+                .title(locale.error_not_found())
+                .description(
+                    locale
+                        .error_not_found_user(&format!("https://scratch.mit.edu/user/{username}")),
+                ),
+            ScratchAPIError::ServerError => embed
+                .color(Color::Error.into())
+                .title(locale.error_scratch_api())
+                .description(locale.error_scratch_api_description()),
+            ScratchAPIError::Other(_) => embed
+                .color(Color::Error.into())
+                .title(locale.error_internal())
+                .description(locale.error_internal_description()),
+        },
+    };
 
     let embed = embed.validate().expect("failed to validate embed").build();
 
