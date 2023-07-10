7bbb6e0a0d17c7a5995132df3654b5d5f44729da
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2023-07-10 17:52:05 +0200

    ✅ don't account for sub-second delay in token test it would be necessary if working with OAuthToken but not with the custom Token type, which uses OffsetDateTime

--- src/database/tests/token.rs
+++ src/database/tests/token.rs
@@ -17,9 +17,7 @@ async fn write_token(pool: PgPool) {
 
     let actual = pool.write_token(id, token).await.unwrap();
 
-    assert_eq!(actual.access_token, expected.access_token);
-    assert_eq!(actual.refresh_token, expected.refresh_token);
-    assert!(actual.expires_at.unix_timestamp() - expected.expires_at.unix_timestamp() <= 1);
+    assert_eq!(actual, expected);
 }
 
 #[sqlx::test(fixtures("linked_accounts"))]
