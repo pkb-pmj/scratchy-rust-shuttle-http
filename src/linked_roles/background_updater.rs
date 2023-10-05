42f4ae94e0722a6861b18a3d2f9df2367dc2d3d2
pkb-pmj <pkb.pmj@gmail.com>
2023-10-05 18:10:20 +0200

    🐛 ♻️ handle no records and all cases better

--- src/linked_roles/background_updater.rs
+++ src/linked_roles/background_updater.rs
@@ -20,7 +20,6 @@ async fn background_updater(state: AppState) -> () {
 
     let start_time = OffsetDateTime::now_utc().time();
     let mut day = interval(Duration::from_secs(60 * 60 * 24));
-    let mut last_updated_at = OffsetDateTime::now_utc();
 
     loop {
         info!("starting today's background metadata update");
@@ -37,12 +36,14 @@ async fn background_updater(state: AppState) -> () {
         // Update cached metadata records starting from the oldest
         // until all records have been updated today
         // Records added today will be alredy up to date on creation
-        while today > last_updated_at {
-            if let Err(err) = update_next_metadata(&state, &mut last_updated_at).await {
-                error!("{}", err);
-                failed += 1;
-            } else {
-                successful += 1;
+        loop {
+            match update_next_metadata(&state, today).await {
+                Ok(Some(())) => successful += 1,
+                Ok(None) => break,
+                Err(err) => {
+                    error!("{}", err);
+                    failed += 1;
+                }
             }
 
             delay.tick().await;
@@ -58,16 +59,19 @@ async fn background_updater(state: AppState) -> () {
 
 async fn update_next_metadata(
     state: &AppState,
-    last_updated_at: &mut OffsetDateTime,
-) -> anyhow::Result<()> {
+    today: OffsetDateTime,
+) -> anyhow::Result<Option<()>> {
     if let Some((id, updated_at)) = state.pool.get_oldest_metadata().await? {
-        debug!("updating role connection metadata for {}", id);
-        state.update_role_connection(id).await?;
-        *last_updated_at = updated_at;
+        if today > updated_at {
+            debug!("updating role connection metadata for {id}");
+            state.update_role_connection(id).await?;
+            Ok(Some(()))
+        } else {
+            debug!("{id} was already updated today at {}", updated_at.time());
+            Ok(None)
+        }
     } else {
         debug!("no records to update");
-        // Prevent infinite loop
-        *last_updated_at = OffsetDateTime::now_utc();
-    };
-    Ok(())
+        Ok(None)
+    }
 }
